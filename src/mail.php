1d22abb969b8d9e2f7012650a921936cc728ccfe
Dmitry <zatulenko@gmail.com>
2018-01-18 22:21:42 +0300

    Изменён скрипт отправки почты

--- src/mail.php
+++ src/mail.php
@@ -1,15 +1,32 @@
 <?php
 
-$recepient = "zatulenko@gmail.com";
-$sitename = "Щенки Хаски - huski-puppies.info";
+$email_in= "zatulenko@gmail.com";
+$email_out= "info@haski-puppies.info";
 
+$sitename = "Щенки Хаски (huski-puppies.info)";
+
+//Удаляем пробелы с начала и конца строки, если таковые имеются
 $type_message = trim($_GET["type_message"]);
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
 $email = trim($_GET["email"]);
 
-$pagetitle = "Новое сообщение с сайта \"$sitename\"";
-$message = "Тип сообщения:$type_message \nИмя: $name \nТелефон: $phone \nE-mail: $email";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+//Преобразуем все спецсимволы, если пользователь попытался добавить их в форму
+$type_message = htmlspecialchars($type_message);
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+$email = htmlspecialchars($email);
+
+//Декодируем url, если пользователь пытался его добавить в форму
+$type_message = urldecode($type_message);
+$name = urldecode($name);
+$phone = urldecode($phone);
+$email = urldecode($email);
+
+
+
+$pagetitle = "Новое сообщение с сайта ".$sitename ;
+$message = "Тип сообщения:".$type_message."\nИмя:".$name."\nТелефон:".$phone."\nE-mail:".$email;
+mail($email_in, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $email_out");
 
 ?>
