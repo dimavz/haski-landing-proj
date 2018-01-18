c5259014f595b01c78b7de46677cb8d930d631a8
Dmitry <zatulenko@gmail.com>
2018-01-18 12:40:04 +0300

    Преобразован скрипт под форму заявки

--- src/mail.php
+++ src/mail.php
@@ -5,8 +5,8 @@ $sitename = "Щенки Хаски - huski-puppies.info";
 
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
+$email = trim($_GET["email"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone \nE-mail: $email";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
