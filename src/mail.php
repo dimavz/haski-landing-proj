91a867b88293f242ad7417984b7c5308c9018886
Dmitry <zatulenko@gmail.com>
2018-01-18 14:37:50 +0300

    Доработан скрипт отправки почты для передачи типа сообщения

--- src/mail.php
+++ src/mail.php
@@ -3,10 +3,11 @@
 $recepient = "zatulenko@gmail.com";
 $sitename = "Щенки Хаски - huski-puppies.info";
 
+$type_message = trim($_GET["type_message"]);
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
 $email = trim($_GET["email"]);
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nE-mail: $email";
+$pagetitle = "Новое сообщение с сайта \"$sitename\"";
+$message = "Тип сообщения:$type_message \nИмя: $name \nТелефон: $phone \nE-mail: $email";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
